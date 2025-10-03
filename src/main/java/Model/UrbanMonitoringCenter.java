1a5b92b0349b6a6d8620925408fcad0a5286975e
Gonzalo Ibarguren <ibargurengonzalo03@gmail.com>
2025-10-03 19:13:52 -0300

    ""

--- src/main/java/Model/UrbanMonitoringCenter.java
+++ src/main/java/Model/UrbanMonitoringCenter.java
@@ -15,17 +15,9 @@ public class UrbanMonitoringCenter{
     private TrafficLightWindows windows;
 
     public UrbanMonitoringCenter() {
-        this.devices = new ArrayList<>();
-        //this.map = new MapWindows();
+        this. devices = new ArrayList<>();
+        this.map = new MapWindows();
        // windows = new TrafficLightWindows(new TrafficLightController());
-
-        TrafficLight light1 = new TrafficLight();
-        TrafficLight light2 = new TrafficLight();
-        TypeInfraction typeInfraction = new TypeInfraction(TypesInfraction.RED_LIGHT,1000,1,"The vehicle ran a red light at the intersection of "+light1.getStreet() +" and "+light2.getStreet());
-        Automobile automobile = new Automobile("AB098AA","Martin Perez","address");
-        EventLocation eventLocation = new EventLocation(1, LocalDateTime.now(),light1.getStreet() + " y "+light2.getStreet());
-        TrafficFine Fine = new TrafficFine(342,1,typeInfraction,automobile,eventLocation,BigDecimal.valueOf(23151.33));
-
 
     }
 
